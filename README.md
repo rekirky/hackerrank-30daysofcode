a3a7c9885815d7a6c390587f7fad4d88b8ff68fc
Kirky <re_kirky@hotmail.com>
2021-07-16 10:16:10 +1000

    Day 0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hackerrank-30daysofcode
  Hackerrank.com - 30 days of code challenge
+Profile page - https://www.hackerrank.com/re_kirky
+Storage of code for the 30 days of code challenge. 
+I am completing this using Python as a learning tool
---
+++ day1.py
@@ -0,0 +1,15 @@
+# Day 1 - Data Types
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+x = int(raw_input())
+y = float(raw_input())
+z = raw_input()
+# Print the sum of both integer variables on a new line.
+print(i+x)
+# Print the sum of the double variables on a new line.
+print(d+y)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+z)
