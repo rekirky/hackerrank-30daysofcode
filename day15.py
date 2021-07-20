96b405ff50a43876e3197fbe8600c0bd363a04f4
Kirky <re_kirky@hotmail.com>
2021-07-20 10:03:34 +1000

    day15

--- day15.py
+++ day15.py
@@ -1,1 +1,24 @@
 # Day 15 - Linked List
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        if head is None:
+            head = Node(data)
+            self.tail = head
+        else: 
+            node = Node(data)
+            self.tail.next = node
+            self.tail = node
+        return head
+mylist= Solution()
