b5f5fb7ebbf0e55aa0578f6c27da3ef8b7778289
Kirky <re_kirky@hotmail.com>
2021-07-21 14:29:23 +1000

    day16
    Hackerrank is shite - original code has an if statement as part of the initial code, this fails their check (don't use if/then loops)
    
    so remove their own code and submit to succeed...

--- day16.py
+++ day16.py
@@ -1,2 +1,18 @@
 # Day 16 - Exceptions - String to Integer
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+S = input()
+
+try:
+    int(S)
+    print(S)
+except:
+    print("Bad String")
+
