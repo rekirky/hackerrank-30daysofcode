8f2b759e3c594056748a8e3e78a7bf6919bdc31c
Kirky <re_kirky@hotmail.com>
2021-07-29 16:21:36 +1000

    day 24
    More nested lists

--- day24.py
+++ day24.py
@@ -1,1 +1,47 @@
 # Day 24 - More Linked Lists
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        values = []
+        if head == None:
+            return 
+        pointer = head
+        values.append(pointer.data)
+        pointer = pointer.next
+        while pointer is not None:
+            values.append(pointer.data)
+            pointer = pointer.next
+        values = list(dict.fromkeys(values))
+        for i in values:
+            print(i,end=" ")
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
