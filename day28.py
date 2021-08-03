b4d5b354e7db0291d62d84258b19da520ed9b87f
Kirky <re_kirky@hotmail.com>
2021-08-04 09:46:57 +1000

    day 28 - regex sorting
    find email addresses and return names. output names in alphabetical order

--- day28.py
+++ day28.py
@@ -1,1 +1,26 @@
 # Day 28 - RegEx, Patterns and Intro to Databases
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    names=[]
+    N = int(input().strip())
+
+    for N_itr in range(N):
+        first_multiple_input = input().rstrip().split()
+
+        firstName = first_multiple_input[0]
+
+        emailID = first_multiple_input[1]
+        if re.search('(@gmail.com)',emailID):
+            names.append(firstName)
+        
+names.sort()
+for i in names:
+    print(i)
+        
