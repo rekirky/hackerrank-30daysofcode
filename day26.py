4a440792bdd2de9a3d99a9b7a46df751f76776ab
Kirky <re_kirky@hotmail.com>
2021-08-03 10:07:49 +1000

    day 26
    check dates to calculate return fees

--- day26.py
+++ day26.py
@@ -1,1 +1,14 @@
 # Day 26 - Nested Logic
+rDay, rMonth, rYear = [int(x) for x in input().split(' ')]
+dDay, dMonth, dYear = [int(x) for x in input().split(' ')]
+
+if (rYear, rMonth, rDay) <= (dYear, dMonth, dDay):
+    print(0)
+elif (rYear, rMonth) == (dYear, dMonth):
+    print(15*(rDay-dDay))
+elif rYear == dYear:
+    print(500*(rMonth-dMonth))
+else:
+    print(10000)
+
+
